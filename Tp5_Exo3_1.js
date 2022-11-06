b2287514f2872e068fb182798c3776ffb6372965
inesbelk <101066324+inesbelk@users.noreply.github.com>
2022-11-06 22:24:34 +0100

    Add files via upload

---
+++ Tp5_Exo3_1.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function matching(regEx,...Files){
+   
+    const regex = new RegExp(regEx);
+    
+    for(txt in Files){
+
+        const isRepertoire = fs.statSync(`./${Files[txt]}`).isDirectory();
+        
+        if(!isRepertoire){
+            const data = fs.readFileSync(`${Files[txt]}`, 'utf8');
+            if(regex.test(data)){
+                
+                return `file name is: ${Files[txt]}`;
+            }
+        }else{
+            let recFiles = fs.readdirSync(Files[txt]);
+            for(file in recFiles){
+                recFiles[file] = `./${Files[txt]}/${recFiles[file]}`;
+            }
+            
+            return matching(regEx,...recFiles);
+        }
+        
+
+    }
+    return "NONE";
+}
+console.log(matching("my",'fileines.txt','filerania.txt'))
